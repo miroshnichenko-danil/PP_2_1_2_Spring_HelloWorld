6f1c9b47956800b91c90a69b6f46be57c840ccd4
Danil <21nash@mail.ru>
2023-04-14 00:51:47 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("1 бин Hello - " + bean.getMessage());
+        System.out.println("2 бин Hello - " + bean.getMessage());
+        System.out.println("Это один и тот же бин? " + (bean == bean2));
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("1 бин Cat - " + bean.getMessage());
+        System.out.println("2 бин Cat - " + bean.getMessage());
+        System.out.println("Это один и тот же бин? " + (catBean == catBean2));
+
     }
 }
